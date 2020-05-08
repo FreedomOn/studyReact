5a9047e9aacc4110d68028e74171e732a64dc35d
FreedomOn <15136616945@163.comm>
2020-05-08 09:47:26 +0800

    jsx语法

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -49,7 +49,8 @@ const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
-
+const lessRegex = /\.less$/;
+const lessModuleRegex = /\.module\.less$/;
 // This is the production and development configuration.
 // It is focused on developer experience, fast rebuilds, and a minimal bundle.
 module.exports = function(webpackEnv) {
@@ -68,7 +69,7 @@ module.exports = function(webpackEnv) {
   const env = getClientEnvironment(paths.publicUrlOrPath.slice(0, -1));
 
   // common function to get style loaders
-  const getStyleLoaders = (cssOptions, preProcessor) => {
+  const getStyleLoaders = (cssOptions,lessOptions, preProcessor) => {
     const loaders = [
       isEnvDevelopment && require.resolve('style-loader'),
       isEnvProduction && {
@@ -82,6 +83,10 @@ module.exports = function(webpackEnv) {
       {
         loader: require.resolve('css-loader'),
         options: cssOptions,
+      },
+      {
+        loader: require.resolve('less-loader'),
+        options: lessOptions,
       },
       {
         // Options for PostCSS as we reference these options twice
@@ -439,8 +444,27 @@ module.exports = function(webpackEnv) {
               // See https://github.com/webpack/webpack/issues/6571
               sideEffects: true,
             },
+
             // Adds support for CSS Modules (https://github.com/css-modules/css-modules)
             // using the extension .module.css
+            {
+              test: lessRegex,
+              exclude: cssModuleRegex,
+              use: getStyleLoaders({
+                importLoaders: 1,
+                sourceMap: isEnvProduction && shouldUseSourceMap,
+              }),
+              sideEffects: true,
+            },
+            {
+              test: lessModuleRegex,
+              use: getStyleLoaders({
+                importLoaders: 1,
+                sourceMap: isEnvProduction && shouldUseSourceMap,
+                modules: true,
+                getLocalIdent: getCSSModuleLocalIdent,
+              }),
+            },
             {
               test: cssModuleRegex,
               use: getStyleLoaders({
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-// import React from 'react';;
-// import ReactDom from 'react-dom';
-import qs from 'qs';
-import './static/less/index.less'  //导入静态背景
-let aa = qs.parse('name=lili&age=18&lx=gril')
-console.log(aa)
+import React from 'react';
+//=>从react-dom中导入一个reactDom,逗号后面的内容是吧reactDom这个对象进行节解构
+import ReactDom ,{render} from 'react-dom';
+
+let root = document.querySelector("#root")
+
+// ReactDom.render(<div  style={{color:'red'}}> 
+//   <h1>我是标题</h1>
+//   </div>,root)
+let styleObj = {
+  color:"red"
+}
+render(<h1 id="titlebox" className="titlle" style={styleObj}>
+  hahaha 
+  </h1>,root)
+/*
+  JSX渲染机制
+    1.基于babel中的语法解析模块（babel-preset-react）把JSX编译为creatElement(...)结构；
+    2.执行react.creatElement(type,props,children),创建一个对象(虚拟DOM)；
+    3.ReactDom.render(jsx语法最后生成的对象，容器)，基于render方法把生成的对象动态创建为dom元素
+      插入到制定的容器中。
+*/ 
